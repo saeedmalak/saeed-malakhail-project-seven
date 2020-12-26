411fe141fb853d185ebf87ebf68ce013617dd5c5
saeedmalak <makhh2001@gmail.com>
2020-12-26 11:45:36 -0500

    menu overlage page is responsive now

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// change nav anchor text when screen size is less than 481px
+if (window.innerWidth <= 480) {
+    // If less than or equal to 480
+    document.querySelector('#services').innerHTML = `Services`;
+}
+
+// same as above, but works on resizing the window dynamically as well
+window.addEventListener("resize", function () {
+    if (window.innerWidth <= 480) {
+        document.querySelector('#services').innerHTML = `services`;
+    }
+    else {
+        document.querySelector('#services').innerHTML = `skills and services`;
+    }
+});
+
 // Switch to light or dark theme on clickEvent
 document.addEventListener('click', function (event) {
 
